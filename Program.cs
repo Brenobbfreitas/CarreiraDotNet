5e62660232c92b128b1e1ad3576d472c144bf880
brenobbfreitas <brenofreitas0000@gmail.com>
2024-02-08 03:36:36 -0300

    motodo construtor de pagamento

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args){
+        var pagamento = new PagamentoCartao();
+        pagamento.Pagar("123");
+    }
+}
+
+
+
+public class Pagamento
+{
+    //metodo constutor
+    public Pagamento()
+    {
+        Console.WriteLine("Iniciando Pagamento");
+    }
+    public virtual void Pagar(string numero)
+    {
+        Console.WriteLine("Pagar");
+    }
+}
+
+public class PagamentoCartao : Pagamento
+{
+    public override void Pagar(string numero)
+    {
+        base.Pagar(numero);
+        Console.WriteLine("Cartão pago ");
+    }
+}
