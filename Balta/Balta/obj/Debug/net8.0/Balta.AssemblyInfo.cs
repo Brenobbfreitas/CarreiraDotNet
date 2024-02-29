c72e20b7edff068c350dd90907d296032edb997e
brenobbfreitas <brenofreitas0000@gmail.com>
2024-02-29 16:16:23 -0300

    switch branchs

--- Balta/Balta/obj/Debug/net8.0/Balta.AssemblyInfo.cs
+++ Balta/Balta/obj/Debug/net8.0/Balta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Balta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c03746cf8cde7a594ca7d380f904fe39f221b54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c596b1241430b3cb78a433f69c669a33c9bd9bcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Balta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Balta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
