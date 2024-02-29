c596b1241430b3cb78a433f69c669a33c9bd9bcb
brenobbfreitas <brenofreitas0000@gmail.com>
2024-02-29 01:37:36 -0300

    delete class and files

--- CreditCardPayment.cs
+++
@@ -1,8 +0,0 @@
-namespace Payments
-{
-    public partial class Payment
-    {
-        public int MyProperty2 { get; set; }
-        //compartilhamento de classes com o partial
-    }
-}
--- Payments.cs
+++
@@ -1,8 +0,0 @@
-namespace Payments
-{
-    public partial class Payment
-    {
-        public int MyProperty1 { get; set; }
-        //compartilhamento de classes com o partial
-    }
-}
